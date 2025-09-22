5b7234b9b10371032bd662d540959dd2e70773f6
mohammad <44770219+twomr1@users.noreply.github.com>
2025-09-22 11:28:47 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 // next.config.js
 const path = require("path");
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.resolve.alias["@" ] = path.resolve(__dirname);
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
 };
